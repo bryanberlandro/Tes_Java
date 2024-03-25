fe45e83ce96844623377f062adcf982ab40b09ae
Berlandro <bryangodlive0@gmail.com>
2024-03-25 08:30:20 +0700

    first commit

---
+++ main.java
@@ -0,0 +1,17 @@
+public class main{
+
+    public static void main(String[] args){
+        // Tipe Data Integer
+        int x = 10;
+        System.out.println("Nilai X = " + x);
+        System.out.println("Nilai Maksimal Integer = " + Integer.MAX_VALUE);
+        System.out.println("Nilai Minimal Integer = " + Integer.MIN_VALUE);
+        System.out.println("Besar Integer = " + Integer.BYTES);
+        System.out.println("Besar Integer = " + Integer.SIZE);
+    
+        char nama = 'b';
+        System.out.println("Hallo Nama saya " + nama);
+        
+    }
+    
+}
